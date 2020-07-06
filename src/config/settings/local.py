231e6ffd222e6189e4f5b691b5561d264632b78a
MatsuiLin101 <matsuilin101@gmail.com>
2020-07-06 17:27:13 +0800

    Celery init config

--- src/config/settings/local.py
+++ src/config/settings/local.py
@@ -10,3 +10,18 @@ DATABASES = {
         'PORT': '5432',
     },
 }
+
+ADMINS = [
+    ('steven', 'tw19900703@hotmail.com'),
+	
+]
+
+SERVER_EMAIL = 'coa.statistics@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'coa.statistics@gmail.com'
+EMAIL_HOST_PASSWORD = 'adminzxcv1234-+'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "COA Statistics <coa.statistics@gmail.com>"
+MANAGERS = ADMINS
