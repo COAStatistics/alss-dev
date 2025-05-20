deadc4f18f9398619c96190b39b0069cdefa0e7e
travishen <travishen.tw@gmail.com>
2025-05-20 20:51:49 +0800

    Fix log admin

--- src/apps/logs/admin.py
+++ src/apps/logs/admin.py
@@ -14,8 +14,8 @@ from .models import ReviewLog
 YEAR_APP_MAP = {}
 APP_YEAR_MAP = {}
 for year, app in zip(
-    ("106", "107", "108", "110", "111", "112"),
-    ("surveys18", "surveys19", "surveys20", "surveys22", "surveys23", "surveys24"),
+    ("106", "107", "108", "110", "111", "112", "113"),
+    ("surveys18", "surveys19", "surveys20", "surveys22", "surveys23", "surveys24", "surveys25"),
 ):
     YEAR_APP_MAP[year] = app
     APP_YEAR_MAP[app] = year
