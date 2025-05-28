98be5bb5c91a6982c2e788bc542ba5b473b494c1
travishen <travishen.tw@gmail.com>
2025-05-28 23:48:16 +0800

    Fix typo

--- src/static/js/surveys25/helper.js
+++ src/static/js/surveys25/helper.js
@@ -1125,7 +1125,7 @@ var LandAreaHelper = {
                 var cropMarketingAreaSum = Object.values(cropMarketingAreaMap).length > 0 ? Object.values(cropMarketingAreaMap).reduce((a,b)=>a+b) : 0;
 
                 var con = landAreaSum < cropMarketingAreaSum;
-                var msg = '【問項1.1】年底耕作地面積總和({0}公畝)應大於或等於【問項1.5】總種植面積({1}公畝)'.format(landAreaSum, cropMarketingAreaSum);
+                var msg = '【問項1.1】年底耕作地面積總和({0}公畝)應大於或等於【問項1.4】總種植面積({1}公畝)'.format(landAreaSum, cropMarketingAreaSum);
                 Helper.LogHandler.Log(con, LandAreaHelper.Alert, msg, this.Guids[0]);
             },
         },
